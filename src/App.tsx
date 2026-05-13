0706803ae37da2b6660d20841a80871e21a774b6
bpcdpc <bpcdpc@hanmail.net>
2026-05-13 16:12:03 +0900

    Install Vite, Tailwindcss, React-leaflet

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import SeoulSan from "./component/SeoulSan";
+
+function App() {
+  return <SeoulSan />;
+}
+
+export default App;
---
+++ src/component/SeoulSan.tsx
@@ -0,0 +1,11 @@
+export default function SeoulSan() {
+  return (
+    <div>
+      <h1>SeoulSan</h1>
+
+      <div className="min-h-screen bg-gray-100 p-6">
+        <h1 className="text-3xl font-bold text-blue-600">Tailwind 4</h1>
+      </div>
+    </div>
+  );
+}
